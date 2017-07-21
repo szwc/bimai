d9e18971786edfcc527f3fb5dba1cb6cbd50ef07
szwc <491157109@qq.com>
2017-07-21 19:37:53 +0800

    first commit fuck

---
+++ index.js
@@ -0,0 +1,13 @@
+
+// $(function () {
+// 	var deviceWidth = $("html").width();
+// 	var fz = deviceWidth/640;
+			    
+
+// 	if(deviceWidth<640){
+// 		$("html").css("font-size",""+100*fz+"px");
+// 	}else{
+// 		$("html").css("font-size","100px");
+// 	}
+				
+// })
